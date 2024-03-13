d877e59186fac7eda8960f95d9482fa9d98afc4a
Yani Pontoni <yani.pontoni@gmail.com>
2024-03-13 15:56:16 -0300

    Update site-header.component.ts
    Minor change

--- src/app/site-header/site-header.component.ts
+++ src/app/site-header/site-header.component.ts
@@ -3,17 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'bot-site-header',
   templateUrl: './site-header.component.html',
-<<<<<<< Updated upstream
   styleUrls: ['./site-header.component.css'],
 })
 export class SiteHeaderComponent {
   constructor() {}
-=======
-  styleUrls: ['./site-header.component.css']
-})
-export class SiteHeaderComponent {
-
-  constructor() { }
-
->>>>>>> Stashed changes
 }
